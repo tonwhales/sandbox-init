b671f63cd077328955b1df33288af821969b9165
Steve Korshakov <steve@korshakov.com>
2022-04-15 16:11:23 +0200

    feat: working create state

--- prepare.ts
+++ prepare.ts
@@ -1,7 +1,27 @@
 import { execSync } from 'child_process';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+import process from 'process';
+import fs from 'fs';
 
-// Create validator keys
-// TODO: Implement
+const INITIAL_VALIDATORS = 3;
 
-// Create zerostate
-execSync('/usr/bin/ton/crypto/create-state -I /usr/src/ton/crypto/fift/lib  -I /usr/src/ton/crypto/smartcont/ ./gen-zerostate.fif', { stdio: 'inherit' });
+(async () => {
+
+    // Create directory
+    rimraf.sync(__dirname + '/zerostate');
+    await mkdirp(__dirname + '/zerostate');
+    process.chdir(__dirname + '/zerostate/');
+
+    // Create validator keys
+    let output: Buffer = Buffer.alloc(0);
+    for (let i = 0; i < INITIAL_VALIDATORS; i++) {
+        execSync('/usr/bin/ton/utils/generate-random-id -m keys -n validator-' + (i + 1));
+        let pub = fs.readFileSync('validator-' + (i + 1) + '.pub').slice(4);
+        output = Buffer.concat([output, pub]);
+    }
+    fs.writeFileSync('validator-keys.pub', output);
+
+    // Create zerostate
+    execSync('/usr/bin/ton/crypto/create-state -I /usr/src/ton/crypto/fift/lib  -I /usr/src/ton/crypto/smartcont/ ../gen-zerostate.fif', { stdio: 'inherit' });
+})();
