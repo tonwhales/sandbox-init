cc3bdb43646de60ebd0756f14126ec93e33f89a7
Steve Korshakov <steve@korshakov.com>
2022-04-15 17:30:18 +0400

    wip: working on key generation

--- prepare.ts
+++ prepare.ts
@@ -1,7 +1,13 @@
 import { execSync } from 'child_process';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+
+// Create directory
+rimraf.sync(__dirname + '/output/');
+mkdirp(__dirname + '/output/');
 
 // Create validator keys
-// TODO: Implement
+execSync('/usr/bin/ton/utils/generate-random-id -m keys -n ');
 
 // Create zerostate
 execSync('/usr/bin/ton/crypto/create-state -I /usr/src/ton/crypto/fift/lib  -I /usr/src/ton/crypto/smartcont/ ./gen-zerostate.fif', { stdio: 'inherit' });
